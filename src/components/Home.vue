aa17ec6200e643254bd8e94d2404cb4cd174d9c7
mangofish <576027405@qq.com>
2021-06-22 22:03:10 +0800

    完成header区域的美化

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,13 @@
 <template>
     <!-- <el-button type="info" @click="logout">退出</el-button> -->
     <el-container class="home-container">
-        <el-header >Header<el-button type="info" @click="logout">退出</el-button></el-header>
+        <el-header >
+            <div>
+                <img src="../assets/logo.png" alt="">
+                <span>电商后台管理系统</span>   
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
         <el-container>
             <el-aside width="200px">Aside</el-aside>
             <el-main>Main</el-main>
@@ -27,6 +33,19 @@ export default {
 //elementUI中的组件名,即为类名
 .el-header {
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+    }
 }
 
 .el-aside {
