28bfda053368821f29c733006fc7a3e979a11497
mangofish <576027405@qq.com>
2021-06-23 15:38:55 +0800

    通过axios拦截器向http请求header头部中添加Authorization字段

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 Vue.prototype.$http = axios
 
+//设置axios请求拦截器，在axios发起所有请求之前，将客户端记录的'token'字段的值，放置到请求的header中的Authorization字段中
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
 //设置axios 发起http请求的根路径
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
 
