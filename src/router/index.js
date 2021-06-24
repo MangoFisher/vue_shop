82bbc8f49a93f339bdf5bdb71eac0ed41b13b8aa
mangofish <576027405@qq.com>
2021-06-24 13:57:29 +0800

    通过给Home组件添加子组件路由的方式,实现用户列表页面的展示

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 import '../assets/css/global.css'
 
 Vue.use(VueRouter)
@@ -22,10 +23,16 @@ const routes = [
     component: Home,
     redirect: '/welcome',
     //Welcome组件作为Home组件的子组件
-    children: [{
-      path: '/welcome',
-      component: Welcome
-    }]
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome
+      },
+      {
+        path: '/users',
+        component: Users
+      }
+    ]
   }
 ]
 
