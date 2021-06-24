3479acb7a3d2762efe452c2ee93a7d263d17a296
mangofish <576027405@qq.com>
2021-06-24 13:42:19 +0800

    Menu开启vue-router属性,激活导航时以 index 作为 path 进行路由跳转,具体可参考https://element.eleme.cn/#/zh-CN/component/menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,13 +11,13 @@
         <el-container>
             <el-aside :width="isCollapse? '64px': '200px'">
                 <div class="troggle-button" @click="toggleCollapse">|||</div>
-                <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :unique-opened="true" :collapse="isCollapse" :collapse-transition="false">
+                <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :unique-opened="true" :collapse="isCollapse" :collapse-transition="false" :router="true">
                     <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
                         <template slot="title">
                             <i :class="iconObj[item.id]"></i>
                             <span>{{ item.authName }}</span>
                         </template>
-                        <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id">
+                        <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
                             <template slot="title">
                                 <i class="el-icon-menu"></i>
                                 <span>{{ subItem.authName }}</span>
