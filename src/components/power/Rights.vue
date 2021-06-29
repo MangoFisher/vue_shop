5121ad8a39f96ddb98c2c3bfe491280e2c7832a8
mangofish <576027405@qq.com>
2021-06-29 20:33:24 +0800

    绘制面包屑导航和卡片视图

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1>这是rights组件</h1>
+    <div>
+    <!-- 面包屑区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+            <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-card>
+            1111
+        </el-card>
+    </div>
 </template>
 
 <script>
