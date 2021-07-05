2d786ab959c840c9609bd5fc031d359de75b4f9e
mangofish <576027405@qq.com>
2021-07-05 19:52:38 +0800

    商品分类页面-通过路由控制,展示商品分类页面

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>这是商品分类组件</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {},
+    created(){}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
