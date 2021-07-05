4399ce6708a31f26d8d64cb9b33652059b52e48b
mangofish <576027405@qq.com>
2021-07-05 23:17:32 +0800

    商品分类页面-初步使用vue-table-with-tree-grid树形控件

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,14 +1,68 @@
 <template>
-    <p>这是商品分类组件</p>
+    <div>
+        <!-- 面包屑区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+        </el-breadcrumb>
+         <!-- 卡片区域 -->
+         <el-card>
+             <el-row>
+                 <el-col>
+                     <el-button type="primary">添加分类</el-button>
+                 </el-col>
+             </el-row>
+             <tree-table :data="cateList" :columns="columns" :selection-type="false" :expand-type="false" :show-index="true" index-text="#" :border="true"></tree-table>
+
+
+             <!-- 分页区域 -->
+            <!-- <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="queryInfo.pagenum"
+                :page-sizes="[1, 2, 5, 10]"
+                :page-size="queryInfo.pagesize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+            </el-pagination> -->
+         </el-card>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+            //商品分类数据列表
+            cateList: [],
+            //商品分类数据总条目数
+            total: 0,
+            queryInfor: {
+                type: 3,
+                pagenum: 1,
+                pagesize: 5
+            },
+            //为TreeTable设定列
+            columns: [
+                {label: '分类名称', prop: 'cate_name'}
+            ]
+        }
     },
-    methods: {},
-    created(){}
+    methods: {
+        //获取商品分类数据
+        async getCateList() {
+            const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
+            // console.log(res)
+            if(res.meta.status !== 200) return this.$message.error('获取商品分类数据失败')
+            this.cateList = res.data
+            this.total = res.data.total
+            console.log(this.total)
+        }
+    },
+    created(){
+        this.getCateList()
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
 
 //将axios引入，并且挂在到全局vue实例中
 import axios from 'axios'
 Vue.prototype.$http = axios
+
+Vue.component('tree-table', TreeTable)
 
 //设置axios请求拦截器，在axios发起所有请求之前，将客户端记录的'token'字段的值，放置到请求的header中的Authorization字段中
 axios.interceptors.request.use(config => {
