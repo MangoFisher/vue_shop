d0071327673c2d280f6fc49017e73b78d3b9e457
mangofish <576027405@qq.com>
2021-07-05 23:50:30 +0800

    商品分类页面-使用自定义模版渲染排序和操作对英烈

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -25,6 +25,17 @@
                              <i class="el-icon-success" v-if="scope.row.cat_deleted == false" style="color:lightgreen;"></i>
                              <i class="el-icon-error" v-else style="color:red;"></i>
                          </template>
+                         <!-- 定义名叫'order'的模版 -->
+                         <template slot="order" slot-scope="scope">
+                             <el-tag v-if="scope.row.cat_level == 0">一级</el-tag>
+                             <el-tag type="success" v-if="scope.row.cat_level == 1">二级</el-tag>
+                             <el-tag type="warning" v-if="scope.row.cat_level == 2">三级</el-tag>
+                         </template>
+                         <!-- 定义名叫'opt'的模版 -->
+                         <template slot="opt" slot-scope="scope">
+                              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+                              <el-button type="danger" icon="el-icon-delete" size="mini" ></el-button>
+                         </template>
              </tree-table>
 
 
@@ -64,6 +75,20 @@ export default {
                     type: 'template',
                     //表示这一列的显示采用一个名叫'isok'的模版
                     template: 'isok'
+                },
+                {
+                    label: '排序', 
+                    //表示这一列是模版列
+                    type: 'template',
+                    //表示这一列的显示采用一个名叫'isok'的模版
+                    template: 'order'
+                },
+                {
+                    label: '操作', 
+                    //表示这一列是模版列
+                    type: 'template',
+                    //表示这一列的显示采用一个名叫'isok'的模版
+                    template: 'opt'
                 }
             ]
         }
