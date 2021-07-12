d89a4f247eeb4bcc33ea3929052ed7af593601b8
mangofish <576027405@qq.com>
2021-07-12 18:19:28 +0800

    商品添加页面-渲染页面的基本机构

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-        <p>商品添加页面</p>
+        <!-- 面包屑区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片视图区域 -->
+        <el-card>
+            <el-alert
+                title="添加商品信息"
+                type="info"
+                center
+                show-icon
+                :closable="false">
+            </el-alert>
+            <!-- 步骤条 -->
+            <el-steps :space="200" :active="1" finish-status="success">
+                <el-step title="已完成"></el-step>
+                <el-step title="进行中"></el-step>
+                <el-step title="步骤 3"></el-step>
+            </el-steps>
+        </el-card>
     </div>
 </template>
 
@@ -11,7 +33,7 @@ export default {
     },
     methods: {},
     created() {
-        
+
     }
 }
 </script>
