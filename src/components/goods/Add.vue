b14110c77d7a3360c7dab13ac5f825dcfe4e4ee8
mangofish <576027405@qq.com>
2021-07-12 18:53:34 +0800

    商品添加页面-实现步骤条和tab标签栏的数据联动效果

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -17,7 +17,7 @@
                 :closable="false">
             </el-alert>
             <!-- 步骤条 -->
-            <el-steps :space="200" :active="activeIndex" finish-status="success" :align-center="true">
+            <el-steps :space="200" :active="activeIndex - 0" finish-status="success" :align-center="true">
                 <el-step title="基本信息"></el-step>
                 <el-step title="商品参数"></el-step>
                 <el-step title="商品属性"></el-step>
@@ -26,12 +26,12 @@
                 <el-step title="完成"></el-step>
             </el-steps>
             <!-- tab栏区域 -->
-            <el-tabs tab-position="left" style="height: 200px;">
-                <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-                <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-                <el-tab-pane label="商品属性">商品属性</el-tab-pane>
-                <el-tab-pane label="商品图片">商品图片</el-tab-pane>
-                <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+            <el-tabs v-model="activeIndex" tab-position="left" style="height: 200px;">
+                <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+                <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+                <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+                <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+                <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
             </el-tabs>
         </el-card>/
     </div>
@@ -42,7 +42,7 @@ export default {
     data() {
         return {
             //步骤条中默认被激活的索引，索引从0开始
-            activeIndex: 0
+            activeIndex: '0'
         }
     },
     methods: {},
