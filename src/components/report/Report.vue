519be0d3b6b147a4e1da985cb19bd21ae06e115b
mangofish <576027405@qq.com>
2021-07-24 22:42:22 +0800

    数据报表页面-通过路由控制显示数据报表页面

---
+++ src/components/report/Report.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+         <!-- 面包屑区域 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+            <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片区域 -->
+        <el-card>
+            
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
